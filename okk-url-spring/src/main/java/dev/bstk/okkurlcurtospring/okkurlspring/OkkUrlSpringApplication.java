cee9708d4dfc485689adcb13db70eca3641db0d6
Bruno Luz <bsilva.se@gmail.com>
2022-08-07 19:34:16 -0300

    main - Adicionando as duas versões de framework

---
+++ okk-url-spring/src/main/java/dev/bstk/okkurlcurtospring/okkurlspring/OkkUrlSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.bstk.okkurlcurtospring.okkurlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkkUrlSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkkUrlSpringApplication.class, args);
+    }
+
+}
---
+++ okk-url-spring/src/test/java/dev/bstk/okkurlcurtospring/okkurlspring/OkkUrlSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bstk.okkurlcurtospring.okkurlspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkkUrlSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
