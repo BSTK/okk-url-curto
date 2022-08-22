6aba8855da0cd2417137256f0d5e64ca8e36a4f4
Bruno Luz <bsilva.se@gmail.com>
2022-08-22 16:35:02 -0300

    main - Adicionando componentes estruturais frontend

---
+++ okk-url-ui/src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule
+  ]
+})
+export class CoreModule { }
