19b98981263de4139dd9ea8c969747fa365da351
Bruno Luz <bsilva.se@gmail.com>
2022-08-22 13:02:39 -0300

    main - Adicionando caso de teste para UrlServiceTest

--- okk-url-spring/src/test/java/dev/bstk/okkurlcurtospring/okkurlspring/domain/UrlServiceTest.java
+++ okk-url-spring/src/test/java/dev/bstk/okkurlcurtospring/okkurlspring/domain/UrlServiceTest.java
@@ -6,6 +6,7 @@ import dev.bstk.okkurlcurtospring.okkurlspring.domain.data.UrlRepository;
 import dev.bstk.okkurlcurtospring.okkurlspring.domain.encoder.QrCode;
 import dev.bstk.okkurlcurtospring.okkurlspring.infra.cache.GerenciadorCache;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,8 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UrlServiceTest {
@@ -33,6 +33,7 @@ class UrlServiceTest {
 
 
     @Test
+    @DisplayName("Deve retornar uma url com dados convertidos")
     void deveRetonarUmaUrlComDadosConvertidos() {
         final var urlMock = new Url();
         urlMock.setId(1L);
@@ -48,6 +49,34 @@ class UrlServiceTest {
         request.setUrl("https://mock-url/ks");
 
         final var urlEncurtada = urlService.encurtar(request);
+
+        Assertions.assertNotNull(urlEncurtada);
+        Assertions.assertNotNull(urlEncurtada.getId());
+        Assertions.assertNotNull(urlEncurtada.getToken());
+        Assertions.assertNotNull(urlEncurtada.getUrlOriginal());
+        Assertions.assertNotNull(urlEncurtada.getUrlEncurtada());
+        Assertions.assertNotNull(urlEncurtada.getUrlEncurtadaQRCode());
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma url com dados convertidos obtidos do cache")
+    void deveRetonarUmaUrlComDadosConvertidosObtidosDoCache() {
+        final var urlCache = new Url();
+        urlCache.setId(1L);
+        urlCache.setToken("token");
+        urlCache.setUrlEncurtadaQRCode("QR_CODE");
+        urlCache.setUrlEncurtada("https://mock-url/token");
+        urlCache.setUrlOriginal("https://mock-url/aaa-aaaaaaa-aaa");
+        when(cache.get(anyInt())).thenReturn(urlCache);
+
+        final var request = new UrlRequest();
+        request.setUrl("https://mock-url/ks");
+
+        final var urlEncurtada = urlService.encurtar(request);
+
+        verifyNoInteractions(repository, qrCode);
+        verify(cache, times(0)).put(anyString(), any(Url.class));
+        verify(cache, times(0)).put(anyInt(), any(Url.class));
 
         Assertions.assertNotNull(urlEncurtada);
         Assertions.assertNotNull(urlEncurtada.getId());
