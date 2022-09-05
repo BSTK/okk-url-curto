e3144a50ebe5f80c7d2a64e95e1ac58d5f6ac8fc
Bruno Luz <bsilva.se@gmail.com>
2022-09-04 21:33:41 -0300

    Corrigindo persistencia no Redis - Quarkus

--- okk-url-quarkus/src/main/java/dev/bstk/okkurlcurtoquarkus/infra/cache/CacheProduce.java
+++ okk-url-quarkus/src/main/java/dev/bstk/okkurlcurtoquarkus/infra/cache/CacheProduce.java
@@ -1,27 +1,31 @@
 package dev.bstk.okkurlcurtoquarkus.infra.cache;
 
-import io.quarkus.arc.Arc;
 import io.quarkus.redis.datasource.ReactiveRedisDataSource;
 import io.quarkus.redis.datasource.RedisDataSource;
 import io.quarkus.runtime.configuration.ProfileManager;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
+@ApplicationScoped
 public class CacheProduce {
 
     private static final String AMBIENTE_DEV = "dev";
     private static final String AMBIENTE_TEST = "test";
+
+    @Inject
+    protected RedisDataSource dataSource;
+
+    @Inject
+    protected ReactiveRedisDataSource reactiveDataSource;
 
     @Produces
     @ApplicationScoped
     public GerenciadorCache cache() {
         return executandoAmbienteDevLocal()
             ? new CacheLocal()
-            : new CacheRedis(
-                Arc.container().instance(RedisDataSource.class).get(),
-                Arc.container().instance(ReactiveRedisDataSource.class).get()
-            );
+            : new CacheRedis(dataSource, reactiveDataSource);
     }
 
     private boolean executandoAmbienteDevLocal() {
--- okk-url-quarkus/src/main/java/dev/bstk/okkurlcurtoquarkus/infra/cache/CacheRedis.java
+++ okk-url-quarkus/src/main/java/dev/bstk/okkurlcurtoquarkus/infra/cache/CacheRedis.java
@@ -1,14 +1,24 @@
 package dev.bstk.okkurlcurtoquarkus.infra.cache;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dev.bstk.okkurlcurtoquarkus.infra.handlerexception.exception.CacheException;
 import io.quarkus.redis.datasource.ReactiveRedisDataSource;
 import io.quarkus.redis.datasource.RedisDataSource;
 import io.quarkus.redis.datasource.keys.ReactiveKeyCommands;
 import io.quarkus.redis.datasource.string.StringCommands;
 
+import java.util.Objects;
+
 public class CacheRedis implements GerenciadorCache {
+
+    private static final String OBJETO_JSON_VALOR = "valor";
+    private static final String OBJETO_JSON_CLASS = "class";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private final StringCommands<String, Object> cache;
     private final ReactiveKeyCommands<String> cacheKeys;
+
 
     public CacheRedis(final RedisDataSource dataSource,
                       final ReactiveRedisDataSource reactiveDataSource) {
@@ -18,12 +28,33 @@ public class CacheRedis implements GerenciadorCache {
 
     @Override
     public Object get(final Object chave) {
-        return cache.get(String.valueOf(chave));
+        try {
+            final var jsonCache = cache.get(String.valueOf(chave));
+            if (Objects.isNull(jsonCache)) {
+                return null;
+            }
+
+            final var json = MAPPER.readValue(jsonCache.toString(), ObjectNode.class);
+            final var jsonValor = json.get(OBJETO_JSON_VALOR).asText();
+            final var jsonClazz = Class.forName(json.get(OBJETO_JSON_CLASS).asText());
+
+            return MAPPER.readValue(jsonValor, jsonClazz);
+        } catch (Exception e) {
+            throw new CacheException("[ Redis ] - Erro ao ler valor do cache!");
+        }
     }
 
     @Override
     public void put(final Object chave, final Object valor) {
-        cache.set(String.valueOf(chave), valor);
+        try {
+            final var json = MAPPER.createObjectNode();
+            json.put(OBJETO_JSON_VALOR, MAPPER.writeValueAsString(valor));
+            json.put(OBJETO_JSON_CLASS, valor.getClass().toString().replace(OBJETO_JSON_CLASS, "").trim());
+
+            cache.set(String.valueOf(chave), MAPPER.writeValueAsString(json));
+        } catch (Exception e) {
+            throw new CacheException("[ Redis ] - Erro ao setar valor no cache!");
+        }
     }
 
     @Override
