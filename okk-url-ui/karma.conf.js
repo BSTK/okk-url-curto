c614ddd89a38ca1424e3fc9f8468986eeabfea02
Bruno Luz <bsilva.se@gmail.com>
2022-09-05 16:36:30 -0300

    Corrigindo configuração frontend e docker

--- okk-url-ui/karma.conf.js
+++ okk-url-ui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/game-okk-ui'),
+      dir: require('path').join(__dirname, './coverage/okk-url-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- okk-url-ui/src/app/app.component.spec.ts
+++ okk-url-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'game-okk-ui'`, () => {
+  it(`should have as title 'okk-url-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('game-okk-ui');
+    expect(app.title).toEqual('okk-url-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('game-okk-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('okk-url-ui app is running!');
   });
 });
